d375b3002fb7814d4df0de1f81b064aab88bc168
Wyatt Roersma <wyattroersma@gmail.com>
2019-06-16 20:38:54 -0700

    Fixed commit

--- config.py
+++ config.py
@@ -30,7 +30,6 @@ class Config(object):
     OBJECT_STORAGE_TYPE = os.environ.get('OBJECT_STORAGE_TYPE') or None
     ALLOWED_EXTENSIONS = os.environ.get('ALLOWED_EXTENSIONS') or ['txt', 'pdf', 'png', 'jpg',
                                                                   'jpeg', 'gif', 'doc', 'docx', 'exe', 'yar', 'zip', '']
-    REDIS_URL = os.environ.get('REDIS_URL') or 'redis://'
     POSTS_PER_PAGE = os.environ.get('POSTS_PER_PAGE') or 25
     ALLOWED_EMAIL_LIST = os.environ.get("ALLOWED_EMAIL_LIST")
     if ALLOWED_EMAIL_LIST:
@@ -46,7 +45,6 @@ class Config(object):
     LDAP_ADMIN = os.environ.get("LDAP_ADMIN") or None
     LDAP_ADMIN_PASSWORD = os.environ.get("LDAP_ADMIN_PASSWORD") or None
     APP_TITLE = os.environ.get('APP_TITLE') or "Analyst Unknown Cyber Range"
-    MONGO_URI = os.environ.get('MONGO_URI') or None
     PRIVACY_POLICY_URL = os.environ.get('PRIVACY_POLICY_URL') or None
     RABBITMQ_SERVER = os.environ.get('RABBITMQ_SERVER') or 'localhost'
     RABBITMQ_PORT = os.environ.get('RABBITMQ_PORT') or '5672'
